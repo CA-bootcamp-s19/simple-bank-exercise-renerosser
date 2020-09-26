9e3e8d0bc5554037da89705c25ec3cf9bea95f5a
renerosser <rene.rosser@gmail.com>
2020-09-26 14:48:13 +0200

    7 pass

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -96,9 +96,13 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-            assert(balances[msg.sender] >= withdrawAmount);
-            balances[msg.sender] -= withdrawAmount;
-           
+            if (withdrawAmount <= balances[msg.sender]) {
+                balances[msg.sender] -= withdrawAmount;
+                msg.sender.transfer(withdrawAmount);
+            }
+            else {
+                revert();
+            }
             emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
             return balances[msg.sender];
     }
